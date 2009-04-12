588f68cbc58b50ab49940202a73b6f1b4de600b6
Anthony Heukmes <anthony.heukmes@skynet.be>
2009-04-12 16:01:57 +0200

    Remove RAILS_ROOT construction

--- install.rb
+++ install.rb
@@ -1,6 +1,3 @@
-# Copy the assets into RAILS_ROOT/public/
-RAILS_ROOT = File.join(File.dirname(__FILE__), '../../../')
- 
 FileUtils.cp_r( 
   Dir[File.join(File.dirname(__FILE__), 'public')], 
   File.join(RAILS_ROOT),
